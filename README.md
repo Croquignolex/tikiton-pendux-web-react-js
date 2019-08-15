0273c366f97d7b3ebc27634e5ea7d64e3358850e
Alex Stéphane <gpetitalex10@gmail.com>
2019-08-15 14:04:45 +0100

    Update read me and dictionary file

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-HOW TO PLAY
+HOW TO PLAY PENDU
+
+You have to guess the random hidden world type choosing each letter
+You have 10s the guess a letter or your overall score will be reduced of 1
+Your overall score is reduced of 2 if you didn't guess the correct letter
+Your overall score added of 3 if you guess a correct letter
+
+The game is over when you guess the correct word 
+You can restart the game and random world will be waiting for you to guess
+
+Thank for playing
+By Tikiton P
--- src/data/words.js
+++ src/data/words.js
@@ -4,7 +4,13 @@ const WORDS = [
     'reality', 'confusion', 'children', 'investigation', 'joy',
     'congratulation', 'graduation', 'examination', 'examinable',
     'terminator', 'disable', 'enable', 'luncher', 'purchase',
-    'locomotion', 'romantic', 'fantastic', 'abomination', 'fun'
+    'locomotion', 'romantic', 'fantastic', 'abomination', 'fun',
+    'car', 'plane', 'elephant', 'tiger', 'lion', 'rabbit', 'sun',
+    'anaconda', 'butterfly', 'vanilla', 'sugar', 'rainbow','turn',
+    'island', 'hypnotic', 'general', 'particular', 'release', 'ice',
+    'aggregation', 'marriage', 'criminal', 'gun', 'electrocution',
+    'reload', 'pocket', 'start', 'ready', 'forgotten', 'explain',
+    'localization', 'geographic', 'extraordinary', 'awesome', 'hug'
 ]
 
 export default WORDS
